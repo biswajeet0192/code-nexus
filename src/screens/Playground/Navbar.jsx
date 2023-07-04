a46958fb5a1784e3af0d300eb6c52543cc7df028
biswajeetbehera123 <107670354+biswajeetbehera123@users.noreply.github.com>
2023-07-04 21:39:25 +0530

    bugs fixed

--- src/screens/Playground/Navbar.jsx
+++ src/screens/Playground/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = ({ isFullScreen }) => {
       }}>
         <Logo src={logo} />
         <MainHeading>
-          <span>Code</span> Deck
+          <span>Code</span> Nexus
         </MainHeading>
       </NavbarContent>
     </NavbarContainer>
